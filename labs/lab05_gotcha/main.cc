608f1e3f41e480f6f302335344b6e0eb5e2b1076
John Strobel <strob105@umn.edu>
2019-02-08 13:41:10 -0600

    add (lab05): adding changes made over the course of the lab

--- labs/lab05_gotcha/main.cc
+++ labs/lab05_gotcha/main.cc
@@ -145,9 +145,9 @@ void ReadyForTransport(Object ** dynamic, Object ** local ) {
   *local = &local_object;
   std::cout << "When uncommented, about to delete." << std::endl;
   // delete *dynamic;
-  // delete dynamic;
-  // delete *local;
-  // delete local;
+  // delete dynamic; //segfault
+  // delete *local; //free(): invalid size
+  // delete local; //double free
 }
 
 void RunMemoryDemo() {
@@ -167,8 +167,8 @@ void RunMemoryDemo() {
     std::cout << "dyn_object address " << (int64_t) dyn_object << std::endl;
     std::cout << std::endl;
   }
-  // delete dyn_object;
-  // delete &dyn_object;
-  // delete local_object;
-  // delete &local_object;
+  // delete dyn_object; //runs
+  // delete &dyn_object; //segfault
+  // delete local_object; //segfault
+  delete &local_object;
 }
