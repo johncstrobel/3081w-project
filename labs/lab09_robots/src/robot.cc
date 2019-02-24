05f8ee1bb2e1db0bc9aa00b88f48ea8857b4c2c3
John Strobel <strob105@umn.edu>
2019-02-24 12:25:14 -0600

    add (lab09): first commit; robot.cc half-built.

---
+++ labs/lab09_robots/src/robot.cc
@@ -0,0 +1,16 @@
+/**
+ * @brief An entity in RobotLand that moves around the window.
+ *
+ * Its id, size, initial position, and velocity are fixed.
+ * The position and direction are updated at each frame refresh.
+ * The color can be changed via a user button in the RobotViewer.
+ *
+ * @file robot.cc
+ */
+
+#include "src/robot.h"
+#include "point.h"
+
+robot::robot(int id, double radius, Point origin, double speed){
+
+}
